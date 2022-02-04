6486bc6eeba97f83df39af5c0da8381a49d0a841
Ali Abdiyev <abdiev@Alis-MacBook-Pro.local>
2022-02-05 01:05:18 +0400

    deploy settings is done

--- emibeauty/settings.py
+++ emibeauty/settings.py
@@ -83,28 +83,12 @@ WSGI_APPLICATION = 'emibeauty.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-if DEBUG == False:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('POSTGRES_DB'),
-            'USER': os.environ.get('POSTGRES_USER'),
-            'PORT': os.environ.get('POSTGRES_PORT'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-            'HOST': os.environ.get('POSTGRES_HOST'),
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'emi_beauty_db',
-            'USER': 'emi_beauty_user',
-            'PORT': 5435,
-            'PASSWORD': 'f26b8ab553cfd4767216dbc7be7702fcad1b0115a6347e87e',
-            'HOST': '127.0.0.1',
-        }
-    }
+}
 
 
 # Password validation
